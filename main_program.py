b7c1b9228d66f815d30a5533f1ac7ea65efb5434
Jan <Jan@uga.edu>
2024-10-30 20:10:43 -0400

    code change

--- main_program.py
+++ main_program.py
@@ -2,15 +2,15 @@ from bankutils import BankAccount
 from bankutils import SavingsAccount
 
 #Bank Account Test Case
-account= BankAccount(-100) # expected error message
-account = BankAccount(100)
+account= BankAccount(-200) # expected error message
+account = BankAccount(200)
 account.deposit(50)
-print(account)# Expected balance: $150
-account.withdraw(200) # Expected: "Insufficient balance"
+print(account)# Expected balance: $250
+account.withdraw(300) # Expected: "Insufficient balance"
 account.withdraw(100)
-print(account) # Expected balance: $5
+print(account) # Expected balance: $200
 #SavingsAccount Test Case
-savings = SavingsAccount(200, 0.02) # 5% interest rate
+savings = SavingsAccount(200, 0.02) # 2% interest rate
 savings.deposit(100) # Expected balance: $300
 savings.apply_interest() # Expected balance after interest: $315
 print(savings) # Expected output: $31
