c54cbeaedfcb24dd59195cc0e843b780f0560ed0
Jan <Jan@uga.edu>
2024-10-31 14:48:21 -0400

    i correct my code and final code

--- main_program.py
+++ main_program.py
@@ -1,18 +1,43 @@
 from bankutils import BankAccount
 from bankutils import SavingsAccount
-
-#Bank Account Test Case
-account= BankAccount(-200) # expected error message
-account = BankAccount(200)
-account.deposit(50)
-print(account)# Expected balance: $250
-account.withdraw(300) # Expected: "Insufficient balance"
-account.withdraw(100)
-print(account) # Expected balance: $200
-#SavingsAccount Test Case
-savings = SavingsAccount(200, 0.02) # 2% interest rate
-savings.deposit(100) # Expected balance: $300
-savings.apply_interest() # Expected balance after interest: $315
-print(savings) # Expected output: $31
 if __name__ == "__main__":
-    main()
+#Bank Account Test Case
+    try:
+        account= BankAccount(-200) # expected error message
+    except ValueError as e:
+        print(e)
+    else:
+        print(account)
+    account = BankAccount(200)
+    account.deposit(50)
+    print(account)# Expected balance: $250
+try:
+    account.withdraw(-200) # expected error message
+except ValueError as e:
+    print(e)
+try:
+    account.withdraw(300) # Expected: "Insufficient balance"
+except ValueError as e:
+    print(e)    
+account.withdraw(100)
+print(account) # Expected balance: $150
+
+#SavingsAccount Test Case
+if __name__ == "__main__":
+    try:
+        Sav_account= SavingsAccount(200,.02) # 2% interest rate
+    except ValueError as e:
+        print(e)
+Sav_account.deposit(100) 
+Sav_account.apply_interest() # Expected balance after interest: $306
+print(Sav_account) 
+try:
+    Sav_account.withdraw(-200) # expected error message
+except ValueError as e:
+    print(e)
+try:
+    Sav_account.withdraw(500) # Expected: "Insufficient balance"
+except ValueError as e:
+    print(e)    
+Sav_account.withdraw(50)
+print(Sav_account)
