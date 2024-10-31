e655eaf7aa63ee8715a5a54b962c23d62d7984b7
Jan <Jan@uga.edu>
2024-10-31 14:53:08 -0400

    i did also chage this part of the code

--- bankutils/account.py
+++ bankutils/account.py
@@ -5,42 +5,43 @@ class BankAccount:
         else:
             self.current_balance = initial_balance
     def deposit(self, amount):
-        if amount < 0:
-            raise ValueError("Invalid input")
-        else:
-            self.current_balance += amount
+        if amount <= 0:
+            raise ValueError( "Invalid Amount")
+        self.current_balance += amount
     def withdraw(self, amount):
-        if amount > self.current_balance:
-            return "Insufficeint funds"
-        else:
-            self.current_balance -= amount
+        if amount <= 0:
+            raise ValueError("Withdrawl cannot be negative")
+        elif amount > self.current_balance:
+            raise ValueError("Insufficeint funds")
+        self.current_balance -= amount
     def check_balance(self):
         return "Your current balance is:" + str(self.current_balance)
     def __str__(self):
-        return self.current_balance
+        return "Bank account: current Balance:$" + str(self.current_balance)
     
-    class SavingsAccount:
-        def __init__(self, initial_balance=0, interest_rate=0.02):
-            if initial_balance <= 0:
-                return ValueError("Amount must be positive")
-            else:
-                self.current_balance = initial_balance
-                self.interest_rate = interest_rate
-        def deposit(self, amount):
-            if amount < 0:
-                return ValueError("Invalid Amount")
-            else:
-                self.current_balance=+amount
-        def withdraw(self,amount):
-            if amount > self.current_balance:
-                return ValueError("Insufficient funds")
-            else:
-                self.amount -= amount
-        def check_balance(self):
-            return "Your current Balance is:"+ str(self.current_balance)
-        def apply_interest(self):
-            self.current_balance*=(1+self.interest_rate)
-            return self.current_balance
-        def __str__(self):
-            return "Saving Account-Current Balance is:"+ str(self.current.balance)
+class SavingsAccount:
+    def __init__(self, initial_balance=0, interest_rate=0.02):
+        if initial_balance < 0:
+            raise ValueError("Amount must be positive")
+        self.current_balance = initial_balance
+        self.interest_rate = interest_rate
+    def deposit(self, amount):
+        if amount <= 0:
+            raise ValueError("Invalid Amount")
+        self.current_balance += amount
+    def withdraw(self,amount):
+        if amount <= 0:
+            raise ValueError("Withdrawl cannot be negative")
+        elif amount > self.current_balance:
+            raise ValueError("Insufficeint funds")
+        self.current_balance -= amount
+    def check_balance(self):
+        return "Your current Balance is:"+ str(self.current_balance)
+    def apply_interest(self):
+        new_rate = self.current_balance*self.interest_rate
+        self.deposit(new_rate)
+    def __str__(self):
+        return "Saving Account-Current Balance is:"+ str(self.current_balance)
+
+
 
